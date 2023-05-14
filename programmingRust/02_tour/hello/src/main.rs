fea05cf390a2941066cfae82da9e3f16a77fe1cc
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-05-14 14:38:26 -0700

    add test and cl args to gcd; init gitignore

--- programmingRust/02_tour/hello/src/main.rs
+++ programmingRust/02_tour/hello/src/main.rs
@@ -1,5 +1,22 @@
+//--------//--------//--------//--------//--------//--------//--------//--------
+use std::env;
+use std::str::FromStr;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER...");
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("GCD({:?} is {}", numbers, d);
 }
 
 
@@ -14,4 +31,11 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         m = m % n;
     }
     n
+}
+
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
 }
