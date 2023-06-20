4d14eb10582c9e860c2979a3d25e0d9f737f3a0f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-06-20 11:31:30 -0700

    update gueessing game

--- rustProgrammingLanguage/02_guessing_game/guessing_game/src/main.rs
+++ rustProgrammingLanguage/02_guessing_game/guessing_game/src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
 
+use rand::Rng;
+
+
 fn main() {
     println!("Guess the number!");
     println!("Guess a number between 1 and 100: ");
+    let number = rand::thread_rng().gen_range(1..=100);
+    //println!("Answer is {number}");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed {guess}");
