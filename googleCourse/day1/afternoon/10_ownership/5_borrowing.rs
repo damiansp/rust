83ba7b6cc0e55693c59339b082e625c2ddbfc0ba
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-06-21 12:43:48 -0700

    borrowing++

--- googleCourse/day1/afternoon/10_ownership/5_borrowing.rs
+++ googleCourse/day1/afternoon/10_ownership/5_borrowing.rs
@@ -3,6 +3,14 @@ fn main() {
     let p2 = Point(10, 20);
     let p3 = add(&p1, &p2);  // ownership of p1, p2 still belongs to main (add "borrows" them)
     println!("{p1:?} + {p2:?} = {p3:?}");
+
+    // can have any number of refs, &T or exactly one mutable ref &mut T
+    let mut a: i32 = 10;
+    let b: &i32 = &a;
+    {
+        let c: &mut i32 = &mut a; // will not compile
+        *c = 20;
+    }
 }
 
 
