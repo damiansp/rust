e68596ab853777f38b068c3c84bc30e62ee18bac
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-06-23 11:38:06 -0700

    loop guessing in guessing game

--- rustProgrammingLanguage/02_guessing_game/guessing_game/src/main.rs
+++ rustProgrammingLanguage/02_guessing_game/guessing_game/src/main.rs
@@ -5,17 +5,20 @@ use rand::Rng;
 
 
 fn main() {
-    println!("Guess the number!");
-    println!("Guess a number between 1 and 100: ");
+    println!("----------Guess the number!----------");
+    println!("Guess a number between 1 and 100");
     let number = rand::thread_rng().gen_range(1..=100);
     //println!("Answer is {number}");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    println!("You guessed {guess}");
-    match guess.cmp(&number) {
-        Ordering::Less => println!("Higher"),
-        Ordering::Greater => println!("Lower"),
-        Ordering::Equal => println!("You win!")
+    loop {
+        println!("Input your guess: > ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        println!("You guessed {guess}");
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Higher"),
+            Ordering::Greater => println!("Lower"),
+            Ordering::Equal => println!("You win!")
+        }
     }
 }
