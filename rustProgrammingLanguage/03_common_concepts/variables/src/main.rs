8f5144e60fe5bf8b0c7ee70644b2d8f69f782b08
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-06-25 12:39:56 -0700

    variables++

--- rustProgrammingLanguage/03_common_concepts/variables/src/main.rs
+++ rustProgrammingLanguage/03_common_concepts/variables/src/main.rs
@@ -1,6 +1,14 @@
+const THREE_HRS_IN_SECS: u32 = 3 * 60 * 60;
+
+
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("x = {x}");
-    x = 6;
+    x += 1;
+    println!("x = {x}");
+    {
+        let x = 2 * x;
+        println!("x (inner) = {x}");
+    }
     println!("x = {x}");
 }
