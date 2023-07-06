2f977ca1396e2f75e27278dc2ccf63f9b4f263e4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-06 16:31:35 -0700

    +lifetime in f calls; variables++

--- rustProgrammingLanguage/03_common_concepts/variables/src/main.rs
+++ rustProgrammingLanguage/03_common_concepts/variables/src/main.rs
@@ -4,7 +4,7 @@ const THREE_HRS_IN_SECS: u32 = 3 * 60 * 60;
 fn main() {
     let mut x = 5;
     println!("x = {x}");
-    x += 1;
+    let x = x + 1;  // no longer mut
     println!("x = {x}");
     {
         let x = 2 * x;
