eaec6b8cf6aba243f0834a60d5c59543991044b6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-08 13:13:11 -0700

    ref safety++

--- programmingRust/05_references/ref_safety.rs
+++ programmingRust/05_references/ref_safety.rs
@@ -1,3 +1,11 @@
 fn main() {
     // Borrowing a local var
+    {
+        let r;
+        {
+            let x = 1;
+            r = &x;
+        }
+        //assert_eq!(*r, 1); // nope, x no longer in scope
+    }   
 }
