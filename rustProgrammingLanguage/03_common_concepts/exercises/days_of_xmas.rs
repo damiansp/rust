9c13e7906f3d37f8e2ecccd7ae227e86be6ec234
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-20 09:39:03 -0700

    +days of xmas

---
+++ rustProgrammingLanguage/03_common_concepts/exercises/days_of_xmas.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh",
+        "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+    let gifts = [
+        "a partridge in a pear tree", "two turtle doves", 
+        "three french hens", "four calling birds", "five golden rings",
+        "six geese a-laying", "seven swans a-swimming",
+        "eight maids a-milking", "nine ladies dancing", 
+        "ten lords a-leaping", "eleven pipers piping", 
+        "twelve drummers drumming"];
+    for i in 0..12 {
+        println!(
+            "On the {} day of Christmas my true love sent to me",
+            days[i]);
+        for j in (0..i+1).rev() {
+            if j == 0 && i != 0 {
+                print!("and ");
+            }
+            println!("{}", gifts[j]);
+        }
+        println!();
+    }
+}
