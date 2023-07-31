69110ca3d07dbb3e2f695d861088e675b721e377
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-30 18:34:03 -0700

    str slices++

--- rustProgrammingLanguage/04_ownership/08_string_slices.rs
+++ rustProgrammingLanguage/04_ownership/08_string_slices.rs
@@ -2,4 +2,16 @@ fn main() {
     let s = String::from("hello world");
     let hello = &s[0..5];   //[..5]
     let world = &s[6..11];  //[6..]
+
+    let first = get_first_word(&s);
+    println!("First: {first}");
+}
+
+
+fn get_first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' { return &s[..i]; }
+    }
+    &s[..]
 }
