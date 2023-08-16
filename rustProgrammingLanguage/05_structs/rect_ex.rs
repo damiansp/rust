7f9237ec144c92acd8cdc282474ddefb0884a41e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-08-16 11:20:25 -0700

    rect +can hold

--- rustProgrammingLanguage/05_structs/rect_ex.rs
+++ rustProgrammingLanguage/05_structs/rect_ex.rs
@@ -3,6 +3,10 @@ fn main() {
     println!("Area: {}", get_area(&rect));
     println!("rect is {:#?}", rect);
     println!("Area again: {}", rect.area());
+    let rect2 = Rectangle{width: 10, height: 40};
+    let rect3 = Rectangle{width: 60, height: 45};
+    println!("rect can hold rect2: {}", rect.can_hold(&rect2));
+    println!("rect can hold rect3: {}", rect.can_hold(&rect3));
 }
 
 
@@ -31,5 +35,9 @@ struct Rectangle {
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
