ce452ced79fdfeac203d043c9513b47e64baa2e4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-08-16 08:51:10 -0700

    rect +area method

--- rustProgrammingLanguage/05_structs/rect_ex.rs
+++ rustProgrammingLanguage/05_structs/rect_ex.rs
@@ -2,6 +2,7 @@ fn main() {
     let rect = Rectangle{width: 30, height: 50};
     println!("Area: {}", get_area(&rect));
     println!("rect is {:#?}", rect);
+    println!("Area again: {}", rect.area());
 }
 
 
@@ -24,4 +25,11 @@ fn get_area(rect: &Rectangle) -> u32 {
 struct Rectangle {
     width: u32,
     height: u32
+}
+
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
