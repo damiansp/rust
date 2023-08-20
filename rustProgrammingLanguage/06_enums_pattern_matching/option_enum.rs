899850c118d26cbb14177c3440e14a0a81b72175
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-08-20 15:35:24 -0700

    +option enum ; g

---
+++ rustProgrammingLanguage/06_enums_pattern_matching/option_enum.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let some_number = Some(5);
+    let missing_number: Option<i32> = None;
+    let some_char = Some('e');
+    let x: i8 = 5
+    //let sum = x + some_number; // error cannot add i8 and Option<i8>
+}
