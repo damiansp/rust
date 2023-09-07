10454088f6ea94e1be148a8c9e81f16c9a2fb643
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-07 08:33:29 -0700

    mk lib funcs public

--- rustProgrammingLanguage/07_packages_crates_modules/restaurant/src/lib.rs
+++ rustProgrammingLanguage/07_packages_crates_modules/restaurant/src/lib.rs
@@ -1,17 +1,17 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        pub fn seat_at_table() {}
     }
 
 
-    mod serving {
-        fn take_order() {}
+    pub mod serving {
+        pub fn take_order() {}
 
-        fn serve_order() {}
-
-        fn take_payment() {}
+        pub fn serve_order() {}
+        
+        pub fn take_payment() {}
     }
 }
 
