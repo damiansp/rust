91bcd87a0019ff6739d936064dcf249b85ec1ca6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-21 08:54:25 -0700

    init func calls

---
+++ programmingRust/06_expressions/10_func_calls.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = gcd(1302, 462);
+    let room = player.location();
+    let mut numbers = Vec::new();
+    server
+        .bind("127.0.0.1:3000")
+        .expect("error binding to address")
+        .run()
+        .expect("error running server");
+    //return Vec<i32>::with_capacity(1000);  // error parsing < 
+    return Vec::<i32>::with_capacity(1000);  // OK
+}
