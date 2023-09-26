17761a46c051985f3659a6a498bc851a432c7c03
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-26 08:30:18 -0700

    vectors++

--- rustProgrammingLanguage/08_common_collections/vectors.rs
+++ rustProgrammingLanguage/08_common_collections/vectors.rs
@@ -13,4 +13,11 @@ fn main() {
         Some(third) => println!("The third element of w is {}", third),
         None => println!("There is no third element.")
     }
+
+    // let does_not_exist = &w[100];
+    // let does_not_exist = w.get(100);
+
+    let mut u = vec![1, 2, 3, 4, 5];
+    let first = &u[0];
+    // u.push(6);  // err: attempting to mutate while holding a reference
 }
