bc2248ab667d5bd4ea7625f5bf90903e03c96813
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-29 13:44:48 -0700

    strings++

--- rustProgrammingLanguage/08_common_collections/strings.rs
+++ rustProgrammingLanguage/08_common_collections/strings.rs
@@ -4,4 +4,11 @@ fn main() {
     let s = data.to_string();  // or let s = "initial contents".to_string();
     let s2 = String::from("initial contents");
     let hello = String::from("السلام عليكم");
+    let mut f = String::from("foo");
+    f.push_str("bar");
+    let mut f2 = String::from("lo");
+    f2.push('l');
+    let h = String::from("Hello, ");
+    let w = String::from("world!");
+    let hw = h + &w; // h has been moved here and can no longer be used
 }
