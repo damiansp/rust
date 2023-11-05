15f3134511552799da9f3333c3d589c1818f252a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-11-05 08:38:08 -0800

    hasmaps++

--- rustProgrammingLanguage/08_common_collections/hashmaps.rs
+++ rustProgrammingLanguage/08_common_collections/hashmaps.rs
@@ -14,4 +14,21 @@ fn main() {
     let field_value = String::from("Blue");
     let mut map = HashMap::new();
     map.insert(field_name, field);  // consumed here
+
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 10);
+    scores2.insert(String::from("Blue"), 25);  // overwrites
+    println!("{:?}", scores2);
+
+    scores2.entry(String::from("Yellow")).or_insert(50);
+    scores2.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores2);
+
+    let txt = "hello world wonderful world";
+    let mut tmap HashMap::new();
+    for word in txt.split_whitespace() {
+        let count = tmap.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", tmap);
 }
