d36aacf067761d5d98e0e712be155b275ce66f42
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-11-23 11:15:22 -0800

    match errs++

--- rustProgrammingLanguage/09_error_handling/matching_on_errors.rs
+++ rustProgrammingLanguage/09_error_handling/matching_on_errors.rs
@@ -1,10 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 
 fn main() {
     let contents_attempt = File::open("hello.txt");
     let contents = match contents_attempt {
         Ok(file) => file,
-        Err(error) => { panic!("Problem opening the file: {:?}", error); }
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                match File::create("hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file: {:?}", e)
+                }
+            }
+            other_error => {
+                panic!("Problem opening the file {:?}", other_error);
+            }
+        }
     };
 }
