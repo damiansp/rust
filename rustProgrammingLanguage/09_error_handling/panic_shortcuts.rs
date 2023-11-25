33dc082d3a40d3d0cbea88754ae4c85537b18478
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-11-25 10:48:53 -0800

    shortcuts++, +err prop

--- rustProgrammingLanguage/09_error_handling/panic_shortcuts.rs
+++ rustProgrammingLanguage/09_error_handling/panic_shortcuts.rs
@@ -3,4 +3,6 @@ use std::fs::File;
 
 fn main() {
     let contents = File::open("hello.txt").unwrap();
+    let take2 = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");  // <- err msg on fail
 }
