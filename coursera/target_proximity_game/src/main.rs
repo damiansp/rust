782b23214248cc9f221b88a9b3e48ad9be01c3f8
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-11-25 23:17:25 -0800

    err prop++; target prox++

--- coursera/target_proximity_game/src/main.rs
+++ coursera/target_proximity_game/src/main.rs
@@ -1,3 +1,7 @@
+//--------//--------//--------//--------//--------//--------//--------//--------
+use std::io;
+
+
 fn main() {
 }
 
@@ -17,4 +21,37 @@ impl Printable for Player {
     fn to_string(&self) -> String {
         format!("{} ({})", self.name, self.score)
     } 
+}
+
+
+fn collect_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        println!("{prompt}");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input")
+        match input.trim().parse() {
+            Ok(val) => return val,
+            Err(_) => continue
+        }
+    }
+}
+
+
+fn collect_players() -> Vec<Player> {
+    let mut players: Vec<Player> = Vec::new();
+    let mut n_players = 0;
+    loop {
+        n_players = collect_input::<u32>("How many plyers (2+)?");
+        if n_players < 2 {
+            println!("Invalid number. Try again.");
+            continue;
+        } else {
+            break;
+        }
+    }
+    for i in 1..=n_players {
+        let name = collect_input(format!("Player {i} name").as_str());
+        players.push(Player{name, score: 0});
+    }
+    players
 }
--- rustProgrammingLanguage/09_error_handling/propagating_errors.rs
+++ rustProgrammingLanguage/09_error_handling/propagating_errors.rs
@@ -19,3 +19,11 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Err(e) => Err(e)
     }
 }
+
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;  // shorthand; same as above
+    let mut username = String::new();
+    username_file.read_to_string(&mut username);
+    Ok(username)
+}
