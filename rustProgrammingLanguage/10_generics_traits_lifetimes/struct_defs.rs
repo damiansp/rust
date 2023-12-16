57a9783ccf682fd950e66ffe13ddd79e124cc393
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-16 11:52:35 -0800

    struct defs++

--- rustProgrammingLanguage/10_generics_traits_lifetimes/struct_defs.rs
+++ rustProgrammingLanguage/10_generics_traits_lifetimes/struct_defs.rs
@@ -1,10 +1,11 @@
 fn main() {
     let integer = Point{x: 5, y:10};
     let float = Point{x: 3.0, y: 4.0};
+    let both = Point{x: 1, y: 2.3};
 }
 
 
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T
+    y: U
 }
