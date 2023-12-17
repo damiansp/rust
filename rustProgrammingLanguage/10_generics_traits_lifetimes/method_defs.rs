5993eedfe499d92e922e22187d8c37dd4d944ec3
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-16 21:27:10 -0800

    methods++

--- rustProgrammingLanguage/10_generics_traits_lifetimes/method_defs.rs
+++ rustProgrammingLanguage/10_generics_traits_lifetimes/method_defs.rs
@@ -1,17 +1,24 @@
 fn main() {
     let p = Point{x: 4, y: 5};
     println!("p.x = {}", p.x());
+    let p2 = Point{x: "Broadway", y: "Main"};
+    let p3 = p1.mixup(p2)
+    println!("p3: x = {}, y = {}", p3.x, p3.y);
 }
 
 
-struct Point<T> {
-    x: T,
-    y: T
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1
 }
 
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
+impl<X1, Y1> Point<X1, Y1> {
+    fn x(&self) -> &X1 {
         &self.x
+    }
+
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y1> {
+        Point {x: self.x, y: other.y}
     }
 }
