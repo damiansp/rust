5bdd7d90b6783afcf4b1af61ae895acce1246df4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-01-15 10:17:21 -0800

    fin lifetime annot; init lifetime elsion

--- rustProgrammingLanguage/10_generics_traits_lifetimes/lifetime_annotations_in_funcs.rs
+++ rustProgrammingLanguage/10_generics_traits_lifetimes/lifetime_annotations_in_funcs.rs
@@ -4,10 +4,21 @@ fn main() {
         let s2 = String::from("longer string is longer");
         let res = longest(s1.as_str(), s2.as_str());
         println!("{res} is longest!");
-    }   
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("No '.' found.");
+    let i = ImportantExcerpt {
+        part: first_sentence
+    };
 }
 
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
+}
+
+
+struct ImportantExcerpt<'a> {
+    part: &'a str
 }
