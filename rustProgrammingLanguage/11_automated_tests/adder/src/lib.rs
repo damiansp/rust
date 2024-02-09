5b36bbeb2102e04dfbc9215d05145d4691925068
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-02-09 13:51:22 -0800

    more ex tests

--- rustProgrammingLanguage/11_automated_tests/adder/src/lib.rs
+++ rustProgrammingLanguage/11_automated_tests/adder/src/lib.rs
@@ -2,6 +2,21 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,18 +31,20 @@ mod tests {
     //fn another() {
     //    panic!("Make this test fail");
     //}
-}
 
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle{width: 8, height: 7};
+        let smaller = Rectangle{width: 5, height: 3};
+        assert!(larger.can_hold(&smaller));
+    }
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32
-}
-
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle{width: 8, height: 7};
+        let smaller = Rectangle{width: 5, height: 3};
+        assert!(!smaller.can_hold(&larger));
     }
 }
+
+
